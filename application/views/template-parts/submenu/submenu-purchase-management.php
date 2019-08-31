8c0acffae8db8f7f469ac0380887bb5ea19aa33c
Sapayth-Lab <sapayth@users.noreply.github.com>
2019-08-31 18:57:27 +0600

    small changes

---
+++ application/views/template-parts/submenu/submenu-purchase-management.php
@@ -0,0 +1,13 @@
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-dashboard"></i> <span>User Management</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-left pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+    <li><a href="<?php echo base_url(); ?>user/add_user"><i class="fa fa-circle-o"></i> Add User</a></li>
+    <li><a href="<?php echo base_url(); ?>user/view_user"><i class="fa fa-circle-o"></i> View Users</a></li>
+    <li><a href="<?php echo base_url(); ?>user/edit_user"><i class="fa fa-circle-o"></i> Edit User</a></li>
+  </ul>
+</li>
