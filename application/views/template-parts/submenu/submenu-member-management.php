0add1949d076050784fb1722057504d478b9b770
Sapayth <sapayth.qubee@gmail.com>
2019-09-09 13:34:02 +0600

    member management static files added

---
+++ application/views/template-parts/submenu/submenu-member-management.php
@@ -0,0 +1,13 @@
+<li class="treeview">
+  <a href="#">
+    <i class="fa fa-dashboard"></i> <span>Members Management</span>
+    <span class="pull-right-container">
+      <i class="fa fa-angle-left pull-right"></i>
+    </span>
+  </a>
+  <ul class="treeview-menu">
+    <li><a href="<?php echo base_url(); ?>member/add_member"><i class="fa fa-circle-o"></i> Add Member</a></li>
+    <li><a href="<?php echo base_url(); ?>member/view_member"><i class="fa fa-circle-o"></i> View Members</a></li>
+    <li><a href="<?php echo base_url(); ?>member/edit_member"><i class="fa fa-circle-o"></i> Edit Member</a></li>
+  </ul>
+</li>
